[Refactor] Refactor (attn & batch) metadata management; Fix precision issue (#6)
* [refactor] refactor (attn & batch) metadata management

* fix: clean up padding; write page table before prepare_metadata

* minor: fill pages

* fix: fix padding issue; don't use resize_

python/minisgl/attention/fa3.py
@@ -7,7 +7,7 @@ import torch
 from minisgl.core import Batch, Req
 
 from .base import BaseAttnBackend, BaseAttnMetadata
-from .utils import BaseCaptureData, make_out_loc, make_positions
+from .utils import BaseCaptureData, make_positions
 
 if TYPE_CHECKING:
     from minisgl.kvcache import BaseKVCache
@@ -27,7 +27,6 @@ class FA3Metadata(BaseAttnMetadata):
     max_seqlen_k: int
     max_seqlen_q: int
 
-    out_loc: torch.Tensor
     page_table: torch.Tensor
 
     def get_positions(self) -> torch.Tensor:
@@ -42,7 +41,6 @@ class FlashAttentionBackend(BaseAttnBackend):
         self.config = config
         self.kvcache = kvcache
         self.capture: FA3CaptureData | None = None
-        self.dummy_req: Req | None = None
         self.max_graph_bs = 0
         self.capture_bs: List[int] = []
         self.scale = config.head_dim**-0.5
@@ -53,7 +51,7 @@ class FlashAttentionBackend(BaseAttnBackend):
     ) -> torch.Tensor:
         metadata = batch.attn_metadata
         assert isinstance(metadata, FA3Metadata)
-        self.kvcache.store_kv(k, v, metadata.out_loc, layer_id)
+        self.kvcache.store_kv(k, v, batch.out_loc, layer_id)
         return _fa3_sgl_impl(
             q=q,
             k_cache=self.kvcache.k_cache(layer_id),
@@ -66,34 +64,16 @@ class FlashAttentionBackend(BaseAttnBackend):
             softmax_scale=self.scale,
         )
 
-    def prepare_metadata(self, batch: Batch, allow_graph: bool) -> None:
-        given_bs = len(batch.reqs)
-        reqs = batch.reqs.copy()
+    def prepare_metadata(self, batch: Batch) -> None:
+        reqs = batch.padded_reqs
 
-        # if we can use the cuda graph, pad the reqs to the next available bs
-        # since batch is not complete (no metadata yet), we can't use `can_use_graph` here
-        if (
-            allow_graph
-            and self.capture is not None
-            and batch.is_decode
-            and given_bs <= self.max_graph_bs
-        ):
-            assert self.dummy_req is not None
-            next_bs = next(bs for bs in self.capture_bs if bs >= given_bs)
-            reqs += [self.dummy_req] * (next_bs - given_bs)
         padded_size = len(reqs)
-        del given_bs
-
         seqlens_q = [req.extend_len for req in reqs]
         seqlens_k = [req.device_len for req in reqs]
         cached_lens = [req.cached_len for req in reqs]
         max_seqlen_k = max(seqlens_k)
         max_seqlen_q = max(seqlens_q)
-        cpu_kwargs = {
-            "device": "cpu",
-            "dtype": torch.int32,
-            "pin_memory": True,
-        }
+        cpu_kwargs = {"device": "cpu", "dtype": torch.int32, "pin_memory": True}
 
         device = self.kvcache.device
         cache_seqlens = torch.tensor(seqlens_k, **cpu_kwargs)
@@ -110,8 +90,6 @@ class FlashAttentionBackend(BaseAttnBackend):
             cu_seqlens_q = cu_seqlens_q.to(self.kvcache.device, non_blocking=True)
 
         positions = make_positions(device, reqs)
-        out_loc = make_out_loc(self.page_table, reqs)
-
         page_table = self.page_table
         new_page_table = torch.stack([page_table[req.table_idx, :max_seqlen_k] for req in reqs])
 
@@ -123,65 +101,45 @@ class FlashAttentionBackend(BaseAttnBackend):
             cache_seqlens=cache_seqlens,
             max_seqlen_k=max_seqlen_k,
             max_seqlen_q=max_seqlen_q,
-            out_loc=out_loc,
             page_table=new_page_table,
         )
-        batch.padded_size = padded_size
 
     def init_capture_graph(self, max_seq_len: int, bs_list: List[int], dummy_req: Req) -> None:
         assert self.capture is None, "Capture already initialized."
         max_bs = max(bs_list)
-        cuda_int_kwargs = {"device": self.kvcache.device, "dtype": torch.int32}
-        capture = FA3CaptureData(
-            input_ids=torch.zeros(max_bs, **cuda_int_kwargs),
-            seq_lens=torch.ones(max_bs, **cuda_int_kwargs),
-            positions=torch.zeros(max_bs, **cuda_int_kwargs),
-            cu_seqlens_k=torch.arange(0, max_bs + 1, **cuda_int_kwargs),
-            cu_seqlens_q=torch.arange(0, max_bs + 1, **cuda_int_kwargs),
-            page_table=torch.zeros((max_bs, max_seq_len), **cuda_int_kwargs),
-            out_loc=torch.zeros(max_bs, **cuda_int_kwargs),
-        )
+        capture = FA3CaptureData.create(max_bs, max_seq_len, self.kvcache.device)
         self.max_graph_bs = max_bs
         self.capture = capture
-        self.dummy_req = dummy_req
         self.capture_bs = sorted(bs_list)
-        assert dummy_req.extend_len == 1, "Dummy req must be for decode."
+        self.dummy_req = dummy_req
 
-    def prepare_for_capture(self, bs: int) -> Batch:
-        assert bs in self.capture_bs and self.capture and self.dummy_req
-        batch = Batch(reqs=[self.dummy_req] * bs, phase="decode")
-
+    def prepare_for_capture(self, batch: Batch) -> None:
+        assert (bs := batch.size) in self.capture_bs and self.capture
         capture = self.capture
         metadata = FA3Metadata(
             cu_seqlens_k=capture.cu_seqlens_k[: bs + 1],
             cu_seqlens_q=capture.cu_seqlens_q[: bs + 1],
             positions=capture.positions[:bs],
             cache_seqlens=capture.seq_lens[:bs],
-            max_seqlen_k=capture.page_table.size(1),  # maximum seqlen k
+            max_seqlen_k=capture.page_table.size(1),
             max_seqlen_q=1,  # decode only
-            out_loc=capture.out_loc[:bs],
             page_table=capture.page_table[:bs, :],
         )
         batch.attn_metadata = metadata
         batch.input_ids = capture.input_ids[:bs]
-        batch.padded_size = bs
-        return batch
+        batch.out_loc = capture.out_loc[:bs]
 
-    def _copy_metadata(self, metadata: FA3Metadata, input_ids: torch.Tensor, bs: int) -> None:
+    def prepare_for_replay(self, batch: Batch) -> None:
+        metadata, bs = batch.attn_metadata, batch.padded_size
+        assert isinstance(metadata, FA3Metadata)
         assert self.capture is not None and bs in self.capture_bs
-        assert len(input_ids) == bs <= self.max_graph_bs
-        self.capture.input_ids[:bs].copy_(input_ids)
+        # cu_seqlens_q is always [0, 1, 2, ..., bs] for decode (i.e. no-op)
+        self.capture.input_ids[:bs].copy_(batch.input_ids)
+        self.capture.out_loc[:bs].copy_(batch.out_loc)
         self.capture.cu_seqlens_k[: bs + 1].copy_(metadata.cu_seqlens_k)
-        # cu_seqlens_q is always [0, 1, 2, ..., bs] for decode
         self.capture.positions[:bs].copy_(metadata.positions)
         self.capture.seq_lens[:bs].copy_(metadata.cache_seqlens)
         self.capture.page_table[:bs, : metadata.max_seqlen_k].copy_(metadata.page_table)
-        self.capture.out_loc[:bs].copy_(metadata.out_loc)
-
-    def prepare_for_replay(self, batch: Batch) -> None:
-        metadata = batch.attn_metadata
-        assert isinstance(metadata, FA3Metadata)
-        self._copy_metadata(metadata, batch.input_ids, batch.padded_size)
 
 
 def _fa3_sgl_impl(
@@ -206,7 +164,7 @@ def _fa3_sgl_impl(
     except ImportError:
         raise ImportError(
             "sgl_kernel.flash_attn is not found. Please install it with `pip install sgl-kernel`.\n"
-            "If you're sure it's correctly installted, try `apt update && apt install libnuma1`."
+            "If you're sure it's correctly installed, try `apt update && apt install libnuma1`."
         )
 
     for x in (k_cache, v_cache, q, page_table, cache_seqlens, cu_seqlens_q, cu_seqlens_k_new):

python/minisgl/attention/fi.py
@@ -13,7 +13,7 @@ from minisgl.utils import divide_even
 from minisgl.utils.logger import init_logger
 
 from .base import BaseAttnBackend, BaseAttnMetadata
-from .utils import BaseCaptureData, make_out_loc, make_positions
+from .utils import BaseCaptureData, make_positions
 
 if TYPE_CHECKING:
     from flashinfer import (
@@ -48,11 +48,9 @@ class FICaptureData(BaseCaptureData):
 @dataclass
 class FIMetadata(BaseAttnMetadata):
     # fmt: off
-    out_loc:            torch.Tensor  # on gpu
     cu_seqlens_q_cpu:   torch.Tensor  # on cpu
     cu_seqlens_k_cpu:   torch.Tensor  # on cpu
-    cu_seqlens_q:       torch.Tensor  # on gpu
-    cu_seqlens_k:       torch.Tensor  # on gpu
+    cu_seqlens_q_gpu:   torch.Tensor  # on gpu
     indices:            torch.Tensor  # on gpu
     last_page_len_cpu:  torch.Tensor  # on cpu
     num_qo_heads:       int
@@ -70,11 +68,9 @@ class FIMetadata(BaseAttnMetadata):
         assert self.page_size == 1, "Currently only page_size=1 is supported."
         assert (
             self.positions.is_cuda
-            and self.out_loc.is_cuda
             and self.cu_seqlens_k_cpu.is_cpu
             and self.cu_seqlens_q_cpu.is_cpu
-            and self.cu_seqlens_q.is_cuda
-            and self.cu_seqlens_k.is_cuda
+            and self.cu_seqlens_q_gpu.is_cuda
             and self.indices.is_cuda
             and self.last_page_len_cpu.is_cpu
             and self.seq_lens_cpu.is_cpu
@@ -84,7 +80,7 @@ class FIMetadata(BaseAttnMetadata):
         return self.positions
 
     def get_last_indices(self, bs: int) -> torch.Tensor:
-        return self.cu_seqlens_q[1 : 1 + bs] - 1
+        return self.cu_seqlens_q_gpu[1 : 1 + bs] - 1
 
 
 class FlashInferBackend(BaseAttnBackend):
@@ -119,16 +115,21 @@ class FlashInferBackend(BaseAttnBackend):
         self.int_workspace_buffer = self.prefill_wrapper._int_workspace_buffer
         self.decode_wrappers._int_workspace_buffer = self.int_workspace_buffer
 
+        # initialize some data members
+        tp_size = get_tp_info().size
+        self.qo_head_local = divide_even(self.config.num_qo_heads, tp_size)
+        self.kv_head_local = divide_even(self.config.num_kv_heads, tp_size)
+
         self.cached_ones_cpu: torch.Tensor = torch.tensor([], dtype=torch.int32, pin_memory=True)
         # for cuda graph
         self.capture_bs: List[int] = []
-        self.dummy_req: Req | None = None
         self.max_graph_bs = 0
         self.graph_wrappers: Dict[int, CUDAGraphBatchDecodeWithPagedKVCacheWrapper] = {}
         self.capture: FICaptureData | None = None
         self.page_table = page_table
 
-    def _initialize_once(self, metadata: FIMetadata) -> None:
+    @staticmethod
+    def _initialize_metadata_once(metadata: FIMetadata) -> None:
         if metadata.initialized:
             return
 
@@ -182,72 +183,39 @@ class FlashInferBackend(BaseAttnBackend):
     ) -> torch.Tensor:
         metadata = batch.attn_metadata
         assert isinstance(metadata, FIMetadata)
-        self._initialize_once(metadata)
-        self.kvcache.store_kv(k, v, metadata.out_loc, layer_id)
-        return metadata.wrapper.run(
-            q=q,
-            paged_kv_cache=(self.kvcache.k_cache(layer_id), self.kvcache.v_cache(layer_id)),
-        )
+        self._initialize_metadata_once(metadata)
+        self.kvcache.store_kv(k, v, batch.out_loc, layer_id)
+        kv_cache = (self.kvcache.k_cache(layer_id), self.kvcache.v_cache(layer_id))
+        return metadata.wrapper.run(q=q, paged_kv_cache=kv_cache)
 
-    def prepare_metadata(self, batch: Batch, allow_graph: bool) -> None:
-        given_bs = len(batch.reqs)
-        reqs = batch.reqs.copy()
+    def prepare_metadata(self, batch: Batch) -> None:
+        reqs = batch.padded_reqs
 
-        # if we can use the cuda graph, pad the reqs to the next available bs
-        # since batch is not complete (no metadata yet), we can't use `can_use_graph` here
-        if (
-            allow_graph
-            and self.capture is not None
-            and batch.is_decode
-            and given_bs <= self.max_graph_bs
-        ):
-            assert self.dummy_req is not None
-            next_bs = next(bs for bs in self.capture_bs if bs >= given_bs)
-            reqs += [self.dummy_req] * (next_bs - given_bs)
         padded_size = len(reqs)
-        del given_bs
-
         seqlens_q = [req.extend_len for req in reqs]
         seqlens_k = [req.device_len for req in reqs]
         cached_lens = [req.cached_len for req in reqs]
         max_seqlen_q = max(seqlens_q)
-        cpu_kwargs = {
-            "device": "cpu",
-            "dtype": torch.int32,
-            "pin_memory": True,
-        }
+        cpu_kwargs = {"device": "cpu", "dtype": torch.int32, "pin_memory": True}
 
         device = self.device
         seq_len_cpu = torch.tensor(seqlens_k, **cpu_kwargs)
         cu_seqlens_k_cpu = torch.tensor([0] + seqlens_k, **cpu_kwargs).cumsum_(dim=0)
-        if max_seqlen_q == 1:
+        if max_seqlen_q == 1:  # decode with all extend_len = 1
             cu_seqlens_q_cpu = torch.arange(0, padded_size + 1, **cpu_kwargs)
         elif all(l == 0 for l in cached_lens):  # prefill with no cache hit
             cu_seqlens_q_cpu = cu_seqlens_k_cpu
         else:  # normal extend prefill, with partial cache hit
             cu_seqlens_q_cpu = torch.tensor([0] + seqlens_q, **cpu_kwargs).cumsum_(dim=0)
-
-        page_table = self.page_table
-
-        positions = make_positions(device, reqs)
-        out_loc = make_out_loc(self.page_table, reqs)
-
-        tp_size = get_tp_info().size
-        qo_head_local = divide_even(self.config.num_qo_heads, tp_size)
-        kv_head_local = divide_even(self.config.num_kv_heads, tp_size)
-
-        # copy from CPU to GPU
         batch.attn_metadata = FIMetadata(
-            positions=positions,
-            out_loc=out_loc,
+            positions=make_positions(device, reqs),
             cu_seqlens_q_cpu=cu_seqlens_q_cpu,
             cu_seqlens_k_cpu=cu_seqlens_k_cpu,
-            cu_seqlens_k=cu_seqlens_k_cpu.to(device, non_blocking=True),
-            cu_seqlens_q=cu_seqlens_q_cpu.to(device, non_blocking=True),
-            indices=torch.cat([page_table[req.table_idx, : req.device_len] for req in reqs]),
+            cu_seqlens_q_gpu=cu_seqlens_q_cpu.to(device, non_blocking=True),
+            indices=torch.cat([self.page_table[req.table_idx, : req.device_len] for req in reqs]),
             last_page_len_cpu=self._get_ones_cpu(padded_size),
-            num_qo_heads=qo_head_local,
-            num_kv_heads=kv_head_local,
+            num_qo_heads=self.qo_head_local,
+            num_kv_heads=self.kv_head_local,
             head_dim=self.config.head_dim,
             page_size=1,
             pos_encoding_mode="NONE",
@@ -255,26 +223,16 @@ class FlashInferBackend(BaseAttnBackend):
             dtype=self.kvcache.dtype,
             wrapper=self.decode_wrappers if batch.is_decode else self.prefill_wrapper,
         )
-        batch.padded_size = padded_size
 
     def init_capture_graph(self, max_seq_len: int, bs_list: List[int], dummy_req: Req) -> None:
         assert self.capture is None, "Capture already initialized."
         max_bs = max(bs_list)
-        cuda_int_kwargs = {"device": self.kvcache.device, "dtype": torch.int32}
-        capture = FICaptureData(
-            input_ids=torch.zeros(max_bs, **cuda_int_kwargs),
-            seq_lens=torch.ones(max_bs, **cuda_int_kwargs),
-            positions=torch.zeros(max_bs, **cuda_int_kwargs),
-            cu_seqlens_k=torch.arange(0, max_bs + 1, **cuda_int_kwargs),
-            cu_seqlens_q=torch.arange(0, max_bs + 1, **cuda_int_kwargs),
-            page_table=torch.zeros((max_bs * max_seq_len), **cuda_int_kwargs),
-            out_loc=torch.zeros(max_bs, **cuda_int_kwargs),
-        )
+        capture = FICaptureData.create(max_bs, max_seq_len, self.kvcache.device)
+        capture.page_table = capture.page_table.view(-1)  # use 1D as ragged indices
         self.max_graph_bs = max_bs
         self.capture = capture
-        self.dummy_req = dummy_req
         self.capture_bs = sorted(bs_list)
-        assert dummy_req.extend_len == 1, "Dummy req must be for decode."
+        self.dummy_req = dummy_req
 
     @cached_property
     def use_tensor_cores(self) -> bool:
@@ -284,13 +242,12 @@ class FlashInferBackend(BaseAttnBackend):
         GQA = self.config.num_qo_heads // self.config.num_kv_heads
         return GQA >= 4
 
-    def prepare_for_capture(self, bs: int) -> Batch:
+    def prepare_for_capture(self, batch: Batch) -> None:
         from flashinfer import CUDAGraphBatchDecodeWithPagedKVCacheWrapper
 
-        assert bs in self.capture_bs and self.capture and self.dummy_req
-        batch = Batch(reqs=[self.dummy_req] * bs, phase="decode")
-
-        assert bs not in self.graph_wrappers
+        bs = batch.size
+        assert bs in self.capture_bs and bs not in self.graph_wrappers and self.capture
+        batch.padded_reqs = batch.reqs
         capture = self.capture
         self.graph_wrappers[bs] = CUDAGraphBatchDecodeWithPagedKVCacheWrapper(
             self.float_workspace_buffer,
@@ -301,27 +258,21 @@ class FlashInferBackend(BaseAttnBackend):
             last_page_len_buffer=capture.one_tensor[:bs],
         )
         self.graph_wrappers[bs]._int_workspace_buffer = self.int_workspace_buffer
-
-        self.prepare_metadata(batch, allow_graph=False)
+        self.prepare_metadata(batch)
         metadata = batch.attn_metadata
         assert isinstance(metadata, FIMetadata)
         metadata.wrapper = self.graph_wrappers[bs]
-        metadata.out_loc = capture.out_loc[:bs]
         metadata.positions = capture.positions[:bs]
         batch.input_ids = capture.input_ids[:bs]
-        self._initialize_once(metadata)
-        return batch
-
-    def _copy_metadata(self, metadata: FIMetadata, input_ids: torch.Tensor, bs: int) -> None:
-        assert self.capture is not None and bs in self.capture_bs
-        assert len(input_ids) == bs <= self.max_graph_bs
-        self.capture.input_ids[:bs].copy_(input_ids)
-        self.capture.positions[:bs].copy_(metadata.positions)
-        self.capture.out_loc[:bs].copy_(metadata.out_loc)
+        batch.out_loc = capture.out_loc[:bs]
+        self._initialize_metadata_once(metadata)
 
     def prepare_for_replay(self, batch: Batch) -> None:
-        metadata = batch.attn_metadata
+        metadata, bs = batch.attn_metadata, batch.padded_size
         assert isinstance(metadata, FIMetadata) and not metadata.initialized
-        self._copy_metadata(metadata, batch.input_ids, batch.padded_size)
-        metadata.wrapper = self.graph_wrappers[batch.padded_size]
-        self._initialize_once(metadata)
+        assert self.capture is not None and bs in self.capture_bs
+        self.capture.input_ids[:bs].copy_(batch.input_ids)
+        self.capture.out_loc[:bs].copy_(batch.out_loc)
+        self.capture.positions[:bs].copy_(metadata.positions)
+        metadata.wrapper = self.graph_wrappers[bs]
+        self._initialize_metadata_once(metadata)

python/minisgl/attention/utils.py
@@ -19,15 +19,18 @@ class BaseCaptureData:
     page_table: torch.Tensor
     out_loc: torch.Tensor
 
-
-def make_out_loc(page_table: torch.Tensor, reqs: List[Req]) -> torch.Tensor:
-    from minisgl.kernel import make_2d_indices
-
-    return make_2d_indices(
-        table_2d=page_table,
-        ranges=[(req.table_idx, req.cached_len, req.device_len) for req in reqs],
-        load_table=True,
-    )
+    @classmethod
+    def create(cls, max_bs: int, max_seq_len: int, device: torch.device, **kwargs):
+        return cls(
+            input_ids=torch.zeros((max_bs,), dtype=torch.int32, device=device),
+            seq_lens=torch.ones((max_bs,), dtype=torch.int32, device=device),
+            positions=torch.zeros((max_bs,), dtype=torch.int32, device=device),
+            cu_seqlens_k=torch.arange(0, max_bs + 1, dtype=torch.int32, device=device),
+            cu_seqlens_q=torch.arange(0, max_bs + 1, dtype=torch.int32, device=device),
+            page_table=torch.zeros((max_bs, max_seq_len), dtype=torch.int32, device=device),
+            out_loc=torch.zeros((max_bs,), dtype=torch.int32, device=device),
+            **kwargs,
+        )
 
 
 def make_positions(device: torch.device, reqs: List[Req]) -> torch.Tensor:

python/minisgl/core.py
@@ -74,10 +74,12 @@ class Batch:
     def __init__(self, *, reqs: List[Req], phase: Literal["prefill", "decode"]):
         self.reqs = reqs
         self.phase: Literal["prefill", "decode"] = phase
-        # these field will be set later by attention backend
-        self.attn_metadata: BaseAttnMetadata
+        # these fields should be set by scheduler
         self.input_ids: torch.Tensor
-        self.padded_size: int  # only != batch_size when this batch uses CUDA graph
+        self.out_loc: torch.Tensor
+        self.padded_reqs: List[Req]  # may contain some dummy reqs for padding
+        # this field should be set by attention backend
+        self.attn_metadata: BaseAttnMetadata
 
     @property
     def is_prefill(self) -> bool:
@@ -91,20 +93,12 @@ class Batch:
     def size(self) -> int:
         return len(self.reqs)
 
+    @property
+    def padded_size(self) -> int:
+        return len(self.padded_reqs)
+
 
 class Context:
-    @staticmethod
-    def create_page_table(
-        max_entries: int,
-        max_seq_len: int,
-        device: torch.device,
-    ) -> torch.Tensor:
-        return torch.zeros(
-            size=(max_entries, max_seq_len),
-            dtype=torch.int32,
-            device=device,
-        )
-
     def __init__(
         self,
         *,

python/minisgl/engine/engine.py
@@ -13,7 +13,7 @@ from minisgl.models import create_model, load_hf_weight
 from minisgl.utils import divide_even, init_logger, torch_dtype
 
 from .config import EngineConfig
-from .graph import GraphRunner
+from .graph import GraphRunner, mem_GB
 from .sample import BatchSamplingArgs, Sampler
 
 logger = init_logger(__name__)
@@ -27,6 +27,10 @@ class ForwardOutput(NamedTuple):
     next_tokens_gpu: torch.Tensor
     next_tokens_cpu: torch.Tensor
     copy_done_event: torch.cuda.Event
+
+
+def create_page_table(shape: Tuple[int, int], device: torch.device) -> torch.Tensor:
+    return torch.zeros(shape, dtype=torch.int32, device=device)
 
 
 class Engine:
@@ -43,18 +47,15 @@ class Engine:
         self.dtype = config.dtype
 
         self.tp_cpu_group = self._init_communication()
-        free_memory = self._sync_get_memory()[1]
-        full_free_memory = free_memory
-        logger.info_rank0(f"Free memory before loading model: {free_memory / (1024**3):.2f} GiB")
+        init_free_memory = self._sync_get_memory()[1]
+        logger.info_rank0(f"Free memory before loading model: {mem_GB(init_free_memory)}")
 
         # load model and determine number of pages
         set_rope_device(self.device)
         with torch.device("meta"), torch_dtype(config.dtype):
             self.model = create_model(config.model_path, config.model_config)
         self.model.load_state_dict(self._load_weight_state_dict())
-        self.num_pages = self._determine_num_pages(free_memory)
-
-        # initialize core data structures
+        self.num_pages = self.dummy_page = self._determine_num_pages(init_free_memory)
         self.kv_cache = create_kvcache(
             num_layers=self.model_config.num_layers,
             num_kv_heads=self.model_config.num_kv_heads,
@@ -63,13 +64,9 @@ class Engine:
             device=self.device,
             dtype=self.dtype,
         )
-
-        free_memory = self._sync_get_memory()[0]
-        logger.info_rank0(f"Free memory after initialization: {free_memory / (1024**3):.2f} GiB")
-
-        max_page_entries = config.max_running_req + 1  # +1 for dummy req
-        self.page_table = Context.create_page_table(
-            max_page_entries, config.max_seq_len, self.device
+        self.page_table = create_page_table(  # + 1 for dummy request
+            (config.max_running_req + 1, config.max_seq_len),
+            device=self.device,
         )
         self.attn_backend = create_attention_backend(
             config.model_config,
@@ -84,10 +81,14 @@ class Engine:
             page_table=self.page_table,
         )
         set_global_ctx(self.ctx)
+        self.sampler = Sampler(self.device)
 
-        # mapping the dummy req to dummy pages
+        post_free_memory = self._sync_get_memory()[0]
+        logger.info_rank0(f"Free memory after initialization: {mem_GB(post_free_memory)}")
+
+        # cuda graph related
         self.dummy_req = Req(
-            input_ids=torch.tensor([0]),
+            input_ids=torch.tensor([0], dtype=torch.int32, device="cpu"),
             table_idx=config.max_running_req,
             cached_len=0,
             output_len=1,
@@ -95,9 +96,7 @@ class Engine:
             sampling_params=None,  # type: ignore
             cache_handle=None,  # type: ignore
         )
-        self.page_table[config.max_running_req].fill_(self.num_pages)
-
-        # cuda graph related
+        self.page_table[self.dummy_req.table_idx].fill_(self.dummy_page)
         self.graph_runner = GraphRunner(
             stream=self.stream,
             device=self.device,
@@ -105,12 +104,11 @@ class Engine:
             attn_backend=self.attn_backend,
             cuda_graph_bs=config.cuda_graph_bs,
             cuda_graph_max_bs=config.cuda_graph_max_bs,
-            free_memory=full_free_memory,  # free memory before loading model
-            dummy_req=self.dummy_req,
+            free_memory=init_free_memory,
             max_seq_len=config.max_seq_len,
             vocab_size=self.model_config.vocab_size,
+            dummy_req=self.dummy_req,
         )
-        self.sampler = Sampler(self.device)
 
     def _init_communication(self) -> torch.distributed.ProcessGroup:
         config = self.config
@@ -191,8 +189,8 @@ class Engine:
         max_free_memory = -int(free_mem_tensor[1].item())
         if max_free_memory - min_free_memory > 2 * 1024 * 1024 * 1024:
             logger.error(
-                f"Memory across TP ranks are imbalanced: min {min_free_memory / (1024**3):.2f} GB, "
-                f"max {max_free_memory / (1024**3):.2f} GB"
+                f"Memory across TP ranks are imbalanced:"
+                f" min {mem_GB(min_free_memory)}, max {mem_GB(max_free_memory)}"
             )
             raise RuntimeError("Memory across TP ranks are imbalanced")
 
@@ -203,10 +201,8 @@ class Engine:
 
         with self.ctx.forward_batch(batch):
             if self.graph_runner.can_use_cuda_graph(batch):
-                logger.debug_rank0("Using CUDA graph")
                 logits = self.graph_runner.replay(batch)
             else:
-                logger.debug_rank0("Not using CUDA graph")
                 logits = self.model.forward()
 
         for req in batch.reqs:
@@ -218,9 +214,6 @@ class Engine:
         copy_done_event = torch.cuda.Event()
         copy_done_event.record(self.stream)
         return ForwardOutput(next_tokens_gpu, next_tokens_cpu, copy_done_event)
-
-    def prepare_batch(self, batch: Batch) -> None:
-        self.attn_backend.prepare_metadata(batch, allow_graph=True)
 
     def shutdown(self) -> None:
         self.graph_runner.destroy_cuda_graphs()

python/minisgl/kvcache/__init__.py
@@ -38,8 +38,7 @@ def create_kvcache(
                 device=device,
                 dtype=dtype,
             )
-        case _:
-            raise ValueError(f"Unsupported KVCacheType: {cache_type}")
+    raise ValueError(f"Unsupported KVCacheType: {cache_type}")
 
 
 def create_cache_manager(device: torch.device, type: str) -> BaseCacheManager:

python/minisgl/scheduler/scheduler.py
@@ -143,33 +143,40 @@ class Scheduler(SchedulerIOMixin):
         if batch is None:
             return None
 
-        self.engine.prepare_batch(batch)
+        # NOTE: Pad the batch if needed
         needed_size = sum(r.extend_len for r in batch.reqs)
+        batch.out_loc = self.cache_manager.allocate(needed_size)
+        padding_size = self.engine.graph_runner.pad_batch(batch)
+        if padding_size > 0:
+            batch.out_loc = torch.nn.functional.pad(
+                batch.out_loc, (0, padding_size), value=self.engine.dummy_page
+            )
+
+        # NOTE: prepare 2d indices for token ids loading and writing
+        new_2d_indices = make_2d_indices(  # NOTE: write to page table before prepare_metadata
+            self.table_manager.page_table,  # NOTE: page_table.shape == token_pool.shape
+            ranges=[(r.table_idx, r.cached_len, r.device_len) for r in batch.padded_reqs],
+            load_table=False,
+            store_value=batch.out_loc,
+        )
+        out_2d_indices = make_2d_indices(
+            self.table_manager.token_pool,
+            ranges=[(r.table_idx, r.device_len, r.device_len + 1) for r in batch.reqs],
+            load_table=False,
+        )
+
+        self.engine.attn_backend.prepare_metadata(batch)
         return ForwardInput(
             batch=batch,
             sample_args=self.engine.sampler.prepare(batch),
-            new_2d_indices=make_2d_indices(
-                self.table_manager.page_table,  # NOTE: page_table.shape == token_pool.shape
-                ranges=[(r.table_idx, r.cached_len, r.device_len) for r in batch.reqs],
-                load_table=False,
-                store_value=self.cache_manager.allocate(needed_size),
-            ),
-            out_2d_indices=make_2d_indices(
-                self.table_manager.token_pool,
-                ranges=[(r.table_idx, r.device_len, r.device_len + 1) for r in batch.reqs],
-                load_table=False,
-                store_value=None,
-            ),
+            new_2d_indices=new_2d_indices,
+            out_2d_indices=out_2d_indices,
         )
 
     def _load_token_ids(self, input: ForwardInput) -> None:
         # NOTE: this function must be called in the engine's forward stream
         batch, new_2d_indices = input.batch, input.new_2d_indices
-        new_tokens = len(new_2d_indices)
-        padded_new_tokens = new_tokens + (batch.padded_size - batch.size)
-        batch.input_ids = torch.empty(padded_new_tokens, dtype=torch.int32, device=self.device)
-        batch.input_ids[:new_tokens] = self.table_manager.token_pool.view(-1)[new_2d_indices]
-        batch.input_ids[new_tokens:].zero_()
+        batch.input_ids = self.table_manager.token_pool.view(-1)[new_2d_indices]
 
     def _write_token_ids(self, input: ForwardInput, output: ForwardOutput) -> None:
         # NOTE: this function must be called in the engine's forward stream
@@ -180,6 +187,14 @@ class Scheduler(SchedulerIOMixin):
         logger.info_rank0("Scheduler is idle, waiting for new reqs...")
         self.cache_manager.check_integrity()
 
+    def _forward(self, forward_input: ForwardInput) -> ForwardOutput:
+        self._load_token_ids(forward_input)
+        batch, sample_args = forward_input.batch, forward_input.sample_args
+        forward_output = self.engine.forward_batch(batch, sample_args)
+        self._write_token_ids(forward_input, forward_output)
+        self.decode_manager.add_reqs(forward_input.batch.reqs)
+        return forward_output
+
     @torch.inference_mode()
     def overlap_loop(self, last_data: ForwardData | None) -> ForwardData | None:
         """
@@ -188,7 +203,6 @@ class Scheduler(SchedulerIOMixin):
         It will overlap the execution of current batch and processing of last batch's results,
         which can effectively hide CPU latency and improve GPU utilization.
         """
-        assert torch.cuda.current_stream() == self.stream
         blocking = not (
             last_data  # don't block if we have a batch to be processed
             or self.prefill_manager.runnable
@@ -197,20 +211,12 @@ class Scheduler(SchedulerIOMixin):
         for msg in self.receive_msg(blocking=blocking):
             self._process_one_msg(msg)
 
-        # schedule this batch
         forward_input = self._schedule_next_batch()
-
-        # run the batch in the engine's forward stream
         ongoing_data = None
         if forward_input is not None:
-            with self.engine_stream_ctx:
+            with self.engine_stream_ctx:  # run the batch in the engine's stream
                 self.engine.stream.wait_stream(self.stream)
-                self._load_token_ids(forward_input)
-                batch, sample_args = forward_input.batch, forward_input.sample_args
-                forward_output = self.engine.forward_batch(batch, sample_args)
-                self._write_token_ids(forward_input, forward_output)
-                self.decode_manager.add_reqs(forward_input.batch.reqs)
-                ongoing_data = (forward_input, forward_output)
+                ongoing_data = (forward_input, self._forward(forward_input))
 
         self._process_last_data(last_data, ongoing_data)
         return ongoing_data
@@ -222,24 +228,20 @@ class Scheduler(SchedulerIOMixin):
             self._process_one_msg(msg)
 
         forward_input = self._schedule_next_batch()
-        self.engine.stream.wait_stream(self.stream)
         ongoing_data = None
         if forward_input is not None:
-            self._load_token_ids(forward_input)
-            batch, sample_args = forward_input.batch, forward_input.sample_args
-            forward_output = self.engine.forward_batch(batch, sample_args)
-            self._write_token_ids(forward_input, forward_output)
-            self.decode_manager.add_reqs(forward_input.batch.reqs)
-            ongoing_data = (forward_input, forward_output)
+            ongoing_data = (forward_input, self._forward(forward_input))
 
         self._process_last_data(ongoing_data, None)
 
     def run_forever(self) -> NoReturn:
         if ENV.DISABLE_OVERLAP_SCHEDULING:
-            torch.cuda.set_stream(self.engine.stream)  # only use engine stream
-            while True:
-                self.normal_loop()
+            with self.engine_stream_ctx:
+                self.engine.stream.wait_stream(self.stream)
+                while True:
+                    self.normal_loop()
         else:
+            assert torch.cuda.current_stream() == self.stream
             data = None
             while True:
                 data = self.overlap_loop(data)